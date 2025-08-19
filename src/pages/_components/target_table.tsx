f4d6f293ee4722045d3d911c52ae49720566e979
kdlprabin <prabinkandel444@gmail.com>
2025-08-19 19:00:11 +0545

    target

--- src/pages/_components/target_table.tsx
+++ src/pages/_components/target_table.tsx
@@ -6,39 +6,83 @@ import axios from "axios";
 const BACKEND_URL = 'https://ems-backend-viey.onrender.com'
 
 
+type DashboardData = {
+  "Branch Name": string;
+  "Staff Code": string;
+  "Staff Name": string;
+  "Post": string;
+  "Position"?: string;
+  "JobsType"?: string;
+  "Monitoring"?: string;
+  "Monitoring Post"?: string;
+  "Indicator"?: string;
+  "Full Marks"?: number;
+  "Shrawan"?: number;
+  "Bhadra"?: number;
+  "Aswoj"?: number;
+  "Kartik"?: number;
+  "Mangsir"?: number;
+  "Poush"?: number;
+  "Magh"?: number;
+  "Falgun"?: number;
+  "Chaitra"?: number;
+  "Baishak"?: number;
+  "Jestha"?: number;
+  "Ashar"?: number;
+  "Total"?: number;
+};
+
+
+const MonthlyData = ({ row }: { row: DashboardData }) => {
+
+  const [indicators, setIndicators] = useState(["Member", "Loanee"]);
+  console.log(row);
+  return (
+    <>
+      {
+        indicators.includes(`${row["Indicator"]}`) ?
+          <>
+            <TableCell>{row["Shrawan"] !== undefined ? Math.round(row["Shrawan"]): "-"}</TableCell>
+            <TableCell>{row["Bhadra"] !== undefined ? Math.round(row["Bhadra"]) : "-"}</TableCell>
+            <TableCell>{row["Aswoj"] !== undefined ? Math.round(row["Aswoj"]) : "-"}</TableCell>
+            <TableCell>{row["Kartik"] !== undefined ? Math.round(row["Kartik"]) : "-"}</TableCell>
+            <TableCell>{row["Mangsir"] !== undefined ? Math.round(row["Mangsir"]) : "-"}</TableCell>
+            <TableCell>{row["Poush"] !== undefined ? Math.round(row["Poush"]) : "-"}</TableCell>
+            <TableCell>{row["Magh"] !== undefined ? Math.round(row["Magh"]) : "-"}</TableCell>
+            <TableCell>{row["Falgun"] !== undefined ? Math.round(row["Falgun"]) : "-"}</TableCell>
+            <TableCell>{row["Chaitra"] !== undefined ? Math.round(row["Chaitra"]) : "-"}</TableCell>
+            <TableCell>{row["Baishak"] !== undefined ? Math.round(row["Baishak"]) : "-"}</TableCell>
+            <TableCell>{row["Jestha"] !== undefined ? Math.round(row["Jestha"]) : "-"}</TableCell>
+            <TableCell>{row["Ashar"] !== undefined ? Math.round(row["Ashar"]) : "-"}</TableCell>
+            <TableCell>{row["Total"] !== undefined ? Math.round(row["Total"]) : "-"}</TableCell>
+          </> :
+          <>
+            <TableCell>{row["Shrawan"] !== undefined ? Number(row["Shrawan"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Bhadra"] !== undefined ? Number(row["Bhadra"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Aswoj"] !== undefined ? Number(row["Aswoj"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Kartik"] !== undefined ? Number(row["Kartik"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Mangsir"] !== undefined ? Number(row["Mangsir"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Poush"] !== undefined ? Number(row["Poush"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Magh"] !== undefined ? Number(row["Magh"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Falgun"] !== undefined ? Number(row["Falgun"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Chaitra"] !== undefined ? Number(row["Chaitra"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Baishak"] !== undefined ? Number(row["Baishak"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Jestha"] !== undefined ? Number(row["Jestha"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Ashar"] !== undefined ? Number(row["Ashar"]).toFixed(2) : "-"}</TableCell>
+            <TableCell>{row["Total"] !== undefined ? Math.round(row["Total"]) : "-"}</TableCell>
+          </>
+      }
+    </>
+  );
+}
+
+
 const TableData = ({ selectedBranch, selectedStaff }: {
   selectedBranch: string | null;
   selectedStaff: string | null;
 }) => {
 
   //fetch dashboard data
-
-  type DashboardData = {
-    "Branch Name": string;
-    "Staff Code": string;
-    "Staff Name": string;
-    "Post": string;
-    "Position"?: string;
-    "JobsType"?: string;
-    "Monitoring"?: string;
-    "Monitoring Post"?: string;
-    "Indicator"?: string;
-    "Full Marks"?: number;
-    "Shrawan"?: number;
-    "Bhadra"?: number;
-    "Aswoj"?: number;
-    "Kartik"?: number;
-    "Mangsir"?: number;
-    "Poush"?: number;
-    "Magh"?: number;
-    "Falgun"?: number;
-    "Chaitra"?: number;
-    "Baishak"?: number;
-    "Jestha"?: number;
-    "Ashar"?: number;
-    "Total"?: number;
-  };
-
   const [dashboard_data, setDashboardData] = useState<DashboardData[]>([]);
 
   useEffect(() => {
@@ -73,19 +117,11 @@ const TableData = ({ selectedBranch, selectedStaff }: {
               <TableCell>{row["Staff Name"]}</TableCell>
               <TableCell>{row["JobsType"]}</TableCell>
               <TableCell>{row["Indicator"]}</TableCell>
-              <TableCell>{row["Shrawan"] !== undefined ? Number(row["Shrawan"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Bhadra"] !== undefined ? Number(row["Bhadra"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Aswoj"] !== undefined ? Number(row["Aswoj"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Kartik"] !== undefined ? Number(row["Kartik"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Mangsir"] !== undefined ? Number(row["Mangsir"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Poush"] !== undefined ? Number(row["Poush"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Magh"] !== undefined ? Number(row["Magh"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Falgun"] !== undefined ? Number(row["Falgun"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Chaitra"] !== undefined ? Number(row["Chaitra"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Baishak"] !== undefined ? Number(row["Baishak"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Jestha"] !== undefined ? Number(row["Jestha"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Ashar"] !== undefined ? Number(row["Ashar"]).toFixed(2) : "-"}</TableCell>
-              <TableCell>{row["Total"]}</TableCell>
+              <>
+                <MonthlyData
+                  row={row}
+                />
+              </>
             </TableRow>
           ))}
     </>
